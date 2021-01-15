af32c21b49274d9beddbfa4798fa921ead23b0c4
jiten2032 <jiten2032@gmail.com>
2021-01-15 22:39:38 +0530

    Patang

--- UI.js
+++ UI.js
@@ -2,30 +2,39 @@ let btn = document.getElementById("SubmitBtn");
 btn.addEventListener('click', MyBtn);
 function MyBtn() {
     // console.log("Hi world")
-
+    RequestType = document.getElementById("RequestType");
+    let Datainput = document.getElementById("DataInput").value;
     let input = document.getElementById("Input").value;
-    let get = document.getElementById("GET").value;
-    let post = document.getElementById("POST").value;
-    let put = document.getElementById("PUT").value;
-    if (get == "GET") {
-        let response = new Response
-        response.ResponseURL(input).then(function(MyData){
+
+
+    let response = new Response
+    if (RequestType.value == "GET") {
+
+        response.GETURL(input).then(function (MyData) {
             let getdata = JSON.stringify(MyData);
-            
-            document.getElementById("PrismResponse").innerHTML = getdata ;
-            // console.log(MyData)
-        }).catch(function(error){
-            document.getElementById("PrismResponse").innerHTML="Please enter a valid URL";
+            // console.log(getdata)
+            document.getElementById("PrismResponse").innerHTML = getdata;
+
+        }).catch(function (error) {
+            document.getElementById("PrismResponse").innerHTML = "Please enter a valid URL";
 
         })
 
-    } else if (post == "POST") {
-        console.log(input)
+    } else if (RequestType.value == "POST") {
+        console.log(Datainput)
+        response.POSTURL(input, Datainput).then(function (responsedata) {
+             
+            console.log(responsedata)
+        
+        }).catch(function (error) {
+            document.getElementById("PrismResponse").innerHTML = "Please enter a valid URL";
 
-    } else if (put == "PUT") {
+        })
+
+    } else if (RequestType.value == "PUT") {
         console.log(input)
     }
-    else{
-        document.getElementById("PrismResponse").innerHTML="";
+    else {
+        document.getElementById("PrismResponse").innerHTML = "";
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-class Response{
-async ResponseURL(url){
-   let ResponseData = await fetch(url);
-   let Data = await ResponseData.json();
-   return Data ; 
-    
-}
+class Response {
+   async GETURL(url) {
+      let ResponseData = await fetch(url, {
+         method: 'GET',
+      })
+      let Data = await ResponseData.json();
+      return Data;
+   }
+   async POSTURL(posturl, resdata) {
+      let ResponsepostData = await fetch(posturl, {
+         method: 'POST',
+         body: JSON.stringify(resdata),
+         headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+         },
+      });
+      let Datapost = await ResponsepostData.json();
+      return Datapost;
+   }
 }
 
+// let post = new GetResponse
+
+// let postdata = {
+//    userId: '1',
+//    id: '1',
+//    title: "my post",
+//    body: "post-1"
+//    }
+//    post.POSTURL('https://jsonplaceholder.typicode.com/users',postdata).then(function(data){
+//       console.log(data)
+//    })
